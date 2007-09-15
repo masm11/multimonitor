7cb9536f6c759d9481a4d5758e40025ed84e9857
Yuuki Harano <masm@flowernet.gr.jp>
2007-09-15 21:27:42 +0900

    ops 1個でグラフを複数表示。

--- src/test.c
+++ src/test.c
@@ -3,26 +3,37 @@
 #include "mccvalue.h"
 #include "ops.h"
 
-static struct ops_t *ops[] = {
+static struct ops_t *ops_list[] = {
     &linux_cpuload_ops,
 };
 
-#define NR (sizeof ops / sizeof ops[0])
+#define NOPS (sizeof ops_list / sizeof ops_list[0])
 
-static void *ptrs[NR];
-static GtkWidget *graph[NR];
+static gint idxs[] = {
+    0, 0,
+};
+
+#define NR (sizeof idxs / sizeof idxs[0])
+
+static GtkWidget *box;
+
+static void update(GtkWidget *widget, gpointer data)
+{
+    struct ops_t *ops = g_object_get_data(G_OBJECT(widget), "mcc-ops");
+    void *ptr = g_object_get_data(G_OBJECT(widget), "mcc-ptr");
+    
+    MccValue *value = (*ops->get)(ptr);
+    mcc_graph_add(MCC_GRAPH(widget), value);
+}
 
 static gboolean timer(gpointer data)
 {
     int i;
     
-    for (i = 0; i < NR; i++)
-	(*ops[i]->sread)();
+    for (i = 0; i < NOPS; i++)
+	(*ops_list[i]->sread)();
     
-    for (i = 0; i < NR; i++) {
-	MccValue *value = (*ops[i]->get)(ptrs[i]);
-	mcc_graph_add(MCC_GRAPH(graph[i]), value);
-    }
+    gtk_container_foreach(GTK_CONTAINER(box), update, NULL);
     
     return TRUE;
 }
@@ -33,17 +44,23 @@ int main(int argc, char **argv)
     
     gtk_init(&argc, &argv);
     
-    for (i = 0; i < NR; i++)
-	(*ops[i]->sinit)();
+    for (i = 0; i < NOPS; i++)
+	(*ops_list[i]->sinit)();
     
     GtkWidget *top = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     
-    for (i = 0; i < NR; i++)
-	ptrs[i] = (*ops[i]->new)();
+    box = gtk_hbox_new(FALSE, 0);
+    gtk_container_add(GTK_CONTAINER(top), box);
     
     for (i = 0; i < NR; i++) {
-	graph[i] = mcc_graph_new((*ops[i]->nvalues)(ptrs[i]));
-	gtk_container_add(GTK_CONTAINER(top), graph[i]);
+	gint idx = idxs[i];
+	struct ops_t *ops = ops_list[idx];
+	void *ptr = (*ops->new)();
+	GtkWidget *g = mcc_graph_new((*ops->nvalues)(ptr));
+	g_object_set_data(G_OBJECT(g), "mcc-ops", ops);
+	g_object_set_data(G_OBJECT(g), "mcc-ptr", ptr);
+	gtk_widget_set_size_request(g, 50, 50);
+	gtk_box_pack_start(GTK_BOX(box), g, FALSE, FALSE, 0);
     }
     
     gtk_widget_show_all(top);
@@ -52,8 +69,8 @@ int main(int argc, char **argv)
     
     gtk_main();
     
-    for (i = 0; i < NR; i++)
-	(*ops[i]->sfini)();
+    for (i = 0; i < NOPS; i++)
+	(*ops_list[i]->sfini)();
     
     return 0;
 }
