063f107d9e70fd8232d5972daf69c5b759a02ace
Yuuki Harano <masm@flowernet.gr.jp>
2007-09-29 22:59:27 +0900

    automake 対応。

---
+++ autogen.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+srcdir=`dirname $0`
+[ -z "$srcdir" ] && srcdir=.
+
+PKG_NAME='multi-monitor'
+REQUIRED_AUTOMAKE_VERSION=1.7
+
+if [ ! -f "$srcdir/src/mccgraph.c" ]; then
+ echo "$srcdir doesn't look like source directory for $PKG_NAME" >&2
+ exit 1
+fi
+
+. macros2/gnome-autogen.sh
