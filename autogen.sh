785102464fefd019dd07e11bc29739adb850c815
Yuuki Harano <masm@flowernet.gr.jp>
2014-04-19 21:32:12 +0900

    warning 修正。 その他修正。

--- autogen.sh
+++ autogen.sh
@@ -5,7 +5,7 @@ srcdir=`dirname $0`
 PKG_NAME='multi-monitor'
 REQUIRED_AUTOMAKE_VERSION=1.7
 
-if [ ! -f "$srcdir/src/mccgraph.c" ]; then
+if [ ! -f "$srcdir/src/loadavg.c" ]; then
  echo "$srcdir doesn't look like source directory for $PKG_NAME" >&2
  exit 1
 fi
--- src/battery.c
+++ src/battery.c
@@ -25,6 +25,7 @@
 #include <fcntl.h>
 #include "types.h"
 #include "sysfs.h"
+#include "list.h"
 #include "battery.h"
 
 #define NBATT 2
